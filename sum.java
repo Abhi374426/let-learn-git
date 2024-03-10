7d766459f12f1726e7189faa4de17545f51a6de6
Abhishek jangid <abhi963696@gmail.com>
2024-03-10 16:52:16 -0700

    add message for div.java

--- sum.java
+++ sum.java
@@ -4,6 +4,6 @@ public class sum {
   public static void main(String[] args) {
     int a=10;
     int b=20;
-    System.out.println("The sum of"+a+"and"+b+"is"+(a+b));
+    System.out.println("The sum of"+a+"and"+b+"is"+(a+b)+"f");
   }  
 }
